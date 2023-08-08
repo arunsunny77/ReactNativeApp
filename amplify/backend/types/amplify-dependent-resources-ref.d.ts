1348b1c68fc5e206a607574aecf57aa03d053d4e
arunsunny77 <arunsunnystar@gmail.com>
2023-08-08 14:46:38 +0530

    Your Message

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "reactnativeapp": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
